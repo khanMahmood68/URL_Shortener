4424b1e269e151d427946e916f4a1e5f47dbedb3
khanMahmood68 <mahmoodashrafkhanm0@gmail.com>
2023-12-22 19:48:34 +0530

    Edit .env

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ import { connectUsingMongoose } from "./src/config/mongooseSchema.js";
 import urlRouter from "./src/features/URL/url.routes.js";
 import jwtAuth from "./src/middleware/jwt.middleware.js";
 
-const port = 3000;
+const port = process.env.port || 3000;
 
 // Create server
 const app = express();
